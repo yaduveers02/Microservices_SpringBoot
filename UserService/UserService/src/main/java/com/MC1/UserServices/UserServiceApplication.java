244d8e6d8a27df5e9ab589496179dae2f57661a3
Yaduveer Singh <yaduveers02@gmail.com>
2023-09-08 08:52:59 +0530

    changes

--- UserService/UserService/src/main/java/com/MC1/UserServices/UserServiceApplication.java
+++ UserService/UserService/src/main/java/com/MC1/UserServices/UserServiceApplication.java
@@ -2,12 +2,17 @@ package com.MC1.UserServices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(UserServiceApplication.class, args);
+		
+		
 	}
 
 }
