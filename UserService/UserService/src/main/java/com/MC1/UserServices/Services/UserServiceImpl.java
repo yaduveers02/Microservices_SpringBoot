26aef0faed758d1d6ef98c375266ce58ee22184e
Yaduveer Singh <yaduveers02@gmail.com>
2023-09-22 08:02:33 +0530

    changes

--- UserService/UserService/src/main/java/com/MC1/UserServices/Services/UserServiceImpl.java
+++ UserService/UserService/src/main/java/com/MC1/UserServices/Services/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package com.MC1.UserServices.Services;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.MC1.UserServices.Entities.User;
 import com.MC1.UserServices.Exceptions.ResourceNotFoundException;
@@ -15,6 +17,9 @@ public class UserServiceImpl implements UserService{
 	@Autowired
 	UserRepository userRepository;
 
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	@Override
 	public User saveUser(User user) {
 //		when we want to get random id as string, used mainly for projects we are not using it as findById(String is not in JpaRepository) 
@@ -33,6 +38,8 @@ public class UserServiceImpl implements UserService{
 	@Override
 	public User getUser(int id) {
 		return userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User with the given id is not present in server : " + id));
+		
+		ArrayList list = restTemplate.getForObject("url", ArrayList.class);
 	}
 
 }
